d82cb83935dbd0ea135b61f4324bc3f4bc6b8b64
Izaias Valentim <izaias451@gmail.com>
2024-05-18 14:40:55 -0300

    feature/ No main será demonstrado a execução dos métodos solicitados no teste. Primeiro iremos obter a lista com todos os funcionários cadastrados.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import data.Data;
+import modelos.Negocio;
 
 public class Main {
     public static void main(String[] args) {
 
+        Data loadData = new Data();
+        var listaFuncionarios = loadData.getFuncionarios();
+
+        Negocio Empresa = new Negocio();
+        Empresa.setFuncionarios(listaFuncionarios);
+        var todosOsFuncionarios = Empresa.getFuncionarios();
+        
     }
 }
