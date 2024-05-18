7fd74a35206b88d086eb517b45ba79da4b5ef086
Izaias Valentim <izaias451@gmail.com>
2024-05-18 14:16:48 -0300

    feature/ Implementado o método que retorna apenas o total pago em benefícios aos funcionários com benefício(Secretários e Vendedores). Há uma verificação caso o Funcionário da iteração seja um Gerente(não possui benefícios). Relativo ao terceiro ponto dos requisitos.

--- src/modelos/Negocio.java
+++ src/modelos/Negocio.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static modelos.Gerente.CARGO_GERENTE;
+
 public class Negocio {
     private Set<Funcionario> funcionarios = new HashSet<Funcionario>();
 
@@ -33,6 +35,15 @@ public class Negocio {
             if (YearMonth.of(ano, mes).isBefore(funcionario.getDataContratacao()))
                 total += 0.0;
             total += funcionario.calculoSalario(mes, ano);
+        }
+        return total;
+    }
+
+    public Double calcularBeneficiosAoMes(List<Funcionario> listaComBeneficio, int mes, int ano) {
+        Double total = 0.0;
+        for (Funcionario funcionario : listaComBeneficio) {
+            if (!funcionario.getCargo().equals(CARGO_GERENTE))
+                total += funcionario.calculoBeneficio(mes, ano);
         }
         return total;
     }
