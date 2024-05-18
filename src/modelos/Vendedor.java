a193a0fbec46c1929a8146eccc53b8e876841e78
Izaias Valentim <izaias451@gmail.com>
2024-05-18 14:01:20 -0300

    feature/ Implementação do calculo do salário do vendedor. calculoSalario() retorna o salário sem o benefício das vendas do mes.

--- src/modelos/Vendedor.java
+++ src/modelos/Vendedor.java
@@ -74,13 +74,14 @@ public class Vendedor extends Funcionario {
     }
 
     @Override
-    public Double calculoBeneficio(int mes, int ano) {
-        // TODO Auto-generated method stub
-        return null;
+    public Double calculoSalario(int mes, int ano) {
+        if (YearMonth.of(ano, mes).isBefore(this.getDataContratacao()))
+            return 0.0;
+        return SALARIO_BASE + calculoAdicionalAoAno(mes, ano);
     }
 
     @Override
-    public Double calculoSalario(int mes, int ano) {
+    public Double calculoBeneficio(int mes, int ano) {
         // TODO Auto-generated method stub
         return null;
     }
