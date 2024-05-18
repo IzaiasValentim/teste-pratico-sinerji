5d4dc4af7e5ce6342d832766887a5383db8346f1
Izaias Valentim <izaias451@gmail.com>
2024-05-18 14:34:43 -0300

    feature/ Adição dos atributos da classe Data.

--- src/data/Data.java
+++ src/data/Data.java
@@ -1,5 +1,18 @@
 package data;
 
+import modelos.Funcionario;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Data {
+    private Set<Funcionario> funcionarios = new HashSet<>();
+
+    public Set<Funcionario> getFuncionarios() {
+        return this.funcionarios;
+    }
+
+    public Data() {
+    }
 
 }
