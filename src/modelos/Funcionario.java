6fddec8c59914a96f42c20c8ce320eaefb271d25
Izaias Valentim <izaias451@gmail.com>
2024-05-18 12:14:12 -0300

    feature/ Definição dos atributos pertencentes a um funcionário. Esta classe é abstrata pois terão outras classes que herdarão suas características e métodos padrões.

--- src/modelos/Funcionario.java
+++ src/modelos/Funcionario.java
@@ -1,4 +1,35 @@
 package modelos;
 
-public class Funcionario {
+import java.time.YearMonth;
+
+public abstract class Funcionario {
+
+    private String nome;
+    private String cargo;
+    private YearMonth dataContratacao;
+    private Double salarioBase;
+
+    public Funcionario(String nome, String cargo, YearMonth dataContratacao, Double salarioBase) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.dataContratacao = dataContratacao;
+        this.salarioBase = salarioBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public YearMonth getDataContratacao() {
+        return dataContratacao;
+    }
+
+    public Double getSalarioBase() {
+        return salarioBase;
+    }
+
 }
