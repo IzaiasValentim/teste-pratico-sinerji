f9b50e749225a5a5a4b380e0f28f1a4abdb26aa2
Izaias Valentim <izaias451@gmail.com>
2024-05-18 13:55:41 -0300

    feature/ Implementação do método que calcula o benefício do secretário e outro método que retorna o salário total.

--- src/modelos/Secretario.java
+++ src/modelos/Secretario.java
@@ -4,7 +4,7 @@ import java.time.YearMonth;
 import java.time.temporal.ChronoUnit;
 
 public class Secretario extends Funcionario {
-    
+
     private static final Double SALARIO_BASE = 7000.0;
     private static final Double ADICIONAL_AO_ANO = 1000.0;
     private static final Double BENEFICIO = 0.20;
@@ -13,7 +13,6 @@ public class Secretario extends Funcionario {
     public Secretario(String nome, YearMonth dataContratacao) {
         super(nome, CARGO_SECRETARIO, dataContratacao, SALARIO_BASE);
     }
-
 
     @Override
     public Double calculoSalario(int mes, int ano) {
@@ -24,14 +23,17 @@ public class Secretario extends Funcionario {
 
     @Override
     public Double calculoBeneficio(int mes, int ano) {
-        // TODO Auto-generated method stub
-        return null;
+        if (YearMonth.of(ano, mes).isBefore(this.getDataContratacao()))
+            return 0.0;
+        return this.calculoSalario(mes, ano) * BENEFICIO;
     }
-    
+
     @Override
     public Double calculoSalarioTotal(int mes, int ano) {
-        // TODO Auto-generated method stub
-        return null;
+        if (YearMonth.of(ano, mes).isBefore(this.getDataContratacao()))
+            return 0.0;
+        Double salarioAtual = calculoSalario(mes, ano);
+        return salarioAtual + calculoBeneficio(mes, ano);
     }
 
     @Override
