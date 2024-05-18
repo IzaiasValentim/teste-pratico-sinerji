341d113a9a600f234e11fafa40c19064c5f8faaa
Izaias Valentim <izaias451@gmail.com>
2024-05-18 14:44:33 -0300

    feature/ Criação de um métodos que filtra de todos os funcionários apenas os Vendedores e os retorna. É feito o mesmo para os Secretarios. A motivação é o uso futuro para métodos que exigem funcionários com benefícios.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import static modelos.Secretario.CARGO_SECRETARIO;
+import static modelos.Vendedor.CARGO_VENDEDOR;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import data.Data;
+import modelos.Funcionario;
 import modelos.Negocio;
+import modelos.Secretario;
+import modelos.Vendedor;
 
 public class Main {
+    
+    public static List<Vendedor> retornaApenasVendedores(List<Funcionario> todosOsFuncionarios) {
+        List<Vendedor> listaDeVendedores = new ArrayList<>();
+        todosOsFuncionarios.forEach(funcionario -> {
+            if (funcionario.getCargo().equals(CARGO_VENDEDOR))
+                listaDeVendedores.add((Vendedor) funcionario);
+        });
+        return listaDeVendedores;
+    }
+
+    public static List<Secretario> retornaApenasSecretarios(List<Funcionario> todosOsFuncionarios) {
+        List<Secretario> listaDeSecretarios = new ArrayList<>();
+        todosOsFuncionarios.forEach(funcionario -> {
+            if (funcionario.getCargo().equals(CARGO_SECRETARIO))
+                listaDeSecretarios.add((Secretario) funcionario);
+        });
+        return listaDeSecretarios;
+    }
+
     public static void main(String[] args) {
 
         Data loadData = new Data();
@@ -10,6 +38,6 @@ public class Main {
         Negocio Empresa = new Negocio();
         Empresa.setFuncionarios(listaFuncionarios);
         var todosOsFuncionarios = Empresa.getFuncionarios();
-        
+
     }
 }
