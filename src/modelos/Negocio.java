7f6840b5d2b3c3cceacc945ac9c9309aa4c169fd
Izaias Valentim <izaias451@gmail.com>
2024-05-18 14:10:08 -0300

    feature/ Em negócio foi definido o Set do tipo Funcionário para armazenar todos os funcionários sem repetição.

--- src/modelos/Negocio.java
+++ src/modelos/Negocio.java
@@ -1,5 +1,20 @@
 package modelos;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Negocio {
-    
+    private Set<Funcionario> funcionarios = new HashSet<Funcionario>();
+
+    public Negocio() {
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios.stream().toList();
+    }
+
+    public void setFuncionarios(Set<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
 }
