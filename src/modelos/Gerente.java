a77d6f8a80c8221d1ec6cc1e1dbde31a1c114ced
Izaias Valentim <izaias451@gmail.com>
2024-05-18 12:19:11 -0300

    feature/ Definida a herança de Funcionário e implementação inicial dos métodos abstratos. Também foi definido os atributos básicos do Gerente e feito seu construtor.

--- src/modelos/Gerente.java
+++ src/modelos/Gerente.java
@@ -1,5 +1,40 @@
 package modelos;
 
-public class Gerente {
+import java.time.YearMonth;
+
+public class Gerente extends Funcionario {
+
+    private static final Double SALARIO_BASE = 20000.0;
+    private static final Double ADICIONAL_AO_ANO = 3000.0;
+    private static final Double BENEFICIO = 0.0;
+    public static final String CARGO_GERENTE = "Gerente";
+
+    public Gerente(String nome, YearMonth dataContratacao) {
+        super(nome, CARGO_GERENTE, dataContratacao, SALARIO_BASE);
+    }
+
+    @Override
+    public Double calculoAdicionalAoAno(int mes, int ano) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Double calculoBeneficio(int mes, int ano) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Double calculoSalario(int mes, int ano) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Double calculoSalarioTotal(int mes, int ano) {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
